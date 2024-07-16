65bfbac673b55d36a7d59747d7c5db4cc1f99cca
MattiaVise <mattiav.doc@gmail.com>
2024-07-17 00:19:32 +0200

    eee

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ const MongoLogger = require("./index")
 
 const run = async () => {
     const logger = new MongoLogger("mongodb://192.168.1.22:27017/loggerDb")
-    await logger.initialize()
+    logger.initialize()
+
     let result = await logger.findLogs({limit: 3, type: "debug"})
     logger.info()
 }

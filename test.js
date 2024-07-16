d0d3fe827bc083b4ac3113ec7edb5dddf1584004
MattiaVise <mattiav.doc@gmail.com>
2024-07-17 01:34:53 +0200

    add license file

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-const MongoLogger = require("./index")
-
-const run = async () => {
-    const logger = new MongoLogger("mongodb://192.168.1.22:27017/loggerDb")
-    await logger.initialize()
-
-    logger.createTemplate("error", "giovanni", "hello {% name %}")
-    console.log(logger.useTemplate("giovnni", {name: "mattia"}))
+async function createLogger() {
+    const logger = new MongoLogger("mongodb://192.168.1.22:27017/loggerDb");
+    try {
+        await logger.initialize();
+        await logger.log('Hello, MongoDB!');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    return logger
 }
 
-run()
+const logger = createLogger();
 
 
